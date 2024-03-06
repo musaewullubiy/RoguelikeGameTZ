9a04dfd43bedd84334d42c7256c219e7a8fc4156
Musaew Ullubii <grasjesdraste@yandex.ru>
2024-03-06 21:01:32 +0300

    First Steps

---
+++ game.py
@@ -0,0 +1,17 @@
+# Тут будут классы
+
+
+class AnimatedActor:
+    def __init__(self):
+        pass
+
+    def on_move(self):
+        pass
+
+    def on_stand(self):
+        pass
+
+    def on_jump(self):
+        pass
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+# Тут будет сам запуск игры
+import pgzrun
+from config import *
+
+
+def draw():
+    pass
+
+
+def update():
+    pass
+
+
+pgzrun.go()
